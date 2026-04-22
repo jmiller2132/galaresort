661a1e3704fae72effac40b59e2273d4b737b26e
jmiller2132 <millerjeff2132@gmail.com>
2026-04-21 19:44:59 -0500

    Fix Sanity studio build: use dynamic import to avoid SSR webpack resolution issues

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,14 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
+"use client";
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import dynamic from "next/dynamic";
 
-export const dynamic = 'force-static'
-
-export { metadata, viewport } from 'next-sanity/studio'
+const Studio = dynamic(() => import("next-sanity/studio").then((mod) => {
+  const { NextStudio } = mod;
+  return import("../../../../sanity.config").then((cfg) => ({
+    default: () => <NextStudio config={cfg.default} />,
+  }));
+}), { ssr: false });
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <Studio />;
 }
